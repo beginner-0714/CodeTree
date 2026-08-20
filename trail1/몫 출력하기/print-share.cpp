c6ddd8e23c97283254932dadede65240bc9e918e
begginer <gihoonisgood@naver.com>
2026-08-20 17:30:57 +0900

    [Passed] 몫 출력하기 (1ms, 0MB)

---
+++ trail1/몫 출력하기/print-share.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,cnt=0;
+    while(1){
+        cin >> n;
+        if(n%2==1){
+            continue;
+        }
+        cout << n/2 << endl;
+        cnt++;
+        if(cnt==3){
+            break;
+        }
+    }
+    return 0;
+}
