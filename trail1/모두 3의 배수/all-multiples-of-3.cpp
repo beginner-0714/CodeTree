6cfeedff4d4af080f0079a040c7f23b9c289d9f6
begginer <gihoonisgood@naver.com>
2026-08-22 16:36:49 +0900

    [Passed] 모두 3의 배수 (1ms, 0MB)

---
+++ trail1/모두 3의 배수/all-multiples-of-3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,cnt=0;
+
+    for(int i=0;i<5;i++){
+        cin >> n;
+        if(n%3==0){
+            cnt++;
+        }
+    }
+
+    if(cnt==5){
+        cout << 1;
+    }
+    else{
+        cout << 0;
+    }
+    return 0;
+}
