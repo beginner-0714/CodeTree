e023d27703ed0f27aad460fbe5f8c646c1cd3c3d
begginer <gihoonisgood@naver.com>
2026-08-22 16:11:01 +0900

    [Passed] ab 사이에 없는 c (2ms, 0MB)

---
+++ trail1/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    bool s = false;
+    cin >> a >> b >> c;
+
+    for(int i=a;i<=b;i++){
+        if(i%c==0){
+            s = true;
+        }
+    }
+
+    if(s == false){
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+    return 0;
+}
