483e3c34ae25af1f66278d91750be131f345ff3e
begginer <gihoonisgood@naver.com>
2026-09-15 13:54:15 +0900

    [Passed] 2가 3번째로 등장하는 위치 (2ms, 0MB)

---
+++ trail1/2가 3번째로 등장하는 위치/where-2-appears-3rd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,arr[100]={},cnt=0;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+        if(arr[i]==2){
+            cnt++;
+        }
+        if(cnt == 3){
+            cout << i+1;
+            break;
+        }
+    }
+    return 0;
+}
