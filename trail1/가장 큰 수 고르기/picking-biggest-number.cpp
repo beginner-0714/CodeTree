5e19e9903353c9e65d5767dd08ccd95c759b0c56
begginer <gihoonisgood@naver.com>
2026-09-15 14:23:34 +0900

    [Passed] 가장 큰 수 고르기 (1ms, 0MB)

---
+++ trail1/가장 큰 수 고르기/picking-biggest-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10]={};
+    int max = 0;
+
+    for(int i=0;i<10;i++){
+        cin >> arr[i];
+        if(arr[i]>max){
+            max = arr[i];
+        }
+    }
+
+    cout << max;
+    return 0;
+}
